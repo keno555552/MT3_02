c2522e5c393e6def0aad45be5334bf1bd234ee35
Kwan Kin Leung <kwankinleung5@gmail.com>
2025-04-18 13:59:26 +0900

    v1.0

--- crashDecision.cpp
+++ crashDecision.cpp
@@ -111,7 +111,10 @@ bool crashDecision(const Sphere& s1, const Plane& s2)
 	Sphere ss1 = s1;
 	Plane ss2 = s2;
 	float distance = Dot(ss1.center, ss2.normal) - ss2.distance;
-	if (distance < ss1.radius) {
+	if (distance < ss1.radius && distance >= 0) {
+		return true;
+	}
+	if (distance > -ss1.radius && distance < 0) {
 		return true;
 	}
 	return false;
